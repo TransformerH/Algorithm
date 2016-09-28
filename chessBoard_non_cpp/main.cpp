1fcc33710e31619b6d900af072a99dd199f58657
Sheryl Han <SherylHan@example.com>
2016-09-28 18:46:52 +0800

    Initial Commit

---
+++ chessBoard_non_cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  chessBoard_non_cpp
+//
+//  Created by 韩雪滢 on 9/28/16.
+//  Copyright © 2016 韩雪滢. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
